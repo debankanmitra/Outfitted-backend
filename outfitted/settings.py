a45447ca91f0267e5b06505e03f4b6a6504f3dde
debankanmitra <debankanmitra@gmail.com>
2024-02-23 23:08:00 +0530

    update

--- outfitted/settings.py
+++ outfitted/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'PORT': '26257',
         'OPTIONS': {
             'sslmode': 'verify-full',
-            'sslrootcert': '/home/devfedora/.postgresql/root.crt',
+            'sslrootcert': os.environ.get('ROOT_CRT'),
         },
     },
 }
