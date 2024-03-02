d39246b134abbe926b5df51f714159e7d5f547e9
debankanmitra <debankanmitra@gmail.com>
2024-03-02 23:28:54 +0530

    functionality for price range filtering

--- outfitted/filters.py
+++ outfitted/filters.py
@@ -5,7 +5,7 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category', lookup_expr='iexact')
     name = django_filters.CharFilter(field_name='name', lookup_expr='iexact')
-    price = django_filters.RangeFilter()
+    price = django_filters.BaseRangeFilter(field_name='price')
     discount = django_filters.NumberFilter(field_name='discount', lookup_expr='gte')
     ratings = django_filters.NumberFilter(field_name='ratings', lookup_expr='gte')
     seller = django_filters.CharFilter(field_name='seller', lookup_expr='iexact')
--- outfitted/models.py
+++ outfitted/models.py
@@ -37,10 +37,10 @@ class Product(models.Model):
     Images = models.CharField(max_length=255, unique=True, blank=True, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.FloatField()
     ratings = models.FloatField()
     buys = models.IntegerField()
-    mrp = models.DecimalField(max_digits=6, decimal_places=2)
+    mrp = models.FloatField()
     discount = models.FloatField()
 
     title = models.TextField(max_length=255,  blank=True)
@@ -86,7 +86,7 @@ class Order(models.Model):
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
-    total_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    total_amount = models.FloatField()
     status = models.CharField(max_length=255)
 
 
@@ -94,7 +94,7 @@ class OrderItem(models.Model):
     id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.FloatField()
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
 
@@ -104,5 +104,5 @@ class Payment(models.Model):
     )
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     payment_date = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.FloatField()
     method = models.CharField(max_length=255)
