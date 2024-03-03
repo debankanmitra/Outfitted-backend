1c0ee7c82141fcd80b3dbca6852f97a4249b7e7c
debankanmitra <debankanmitra@gmail.com>
2024-03-03 23:58:01 +0530

    FILTERING MULTIPLE NAMES

--- outfitted/filters.py
+++ outfitted/filters.py
@@ -1,14 +1,19 @@
 # filters.py
+from re import split
 import django_filters
 from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category', lookup_expr='iexact')
-    name = django_filters.CharFilter(field_name='name', lookup_expr='iexact')
+    name = django_filters.CharFilter(field_name='name', method='filter_names')
     price = django_filters.BaseRangeFilter(field_name='price')
     discount = django_filters.NumberFilter(field_name='discount', lookup_expr='gte')
     ratings = django_filters.NumberFilter(field_name='ratings', lookup_expr='gte')
     seller = django_filters.CharFilter(field_name='seller', lookup_expr='iexact')
+
+    def filter_names(self, queryset, name, value):
+        names = value.split(',')
+        return queryset.filter(name__in=names)
 
     class Meta:
         model = Product
