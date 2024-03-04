a8a7a1a3ac859774212e0253d2e349c52982c91d
debankanmitra <debankanmitra@gmail.com>
2024-03-04 15:55:21 +0530

    FILTERING MULTIPLE category

--- outfitted/filters.py
+++ outfitted/filters.py
@@ -4,7 +4,7 @@ import django_filters
 from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
-    category = django_filters.CharFilter(field_name='category', lookup_expr='iexact')
+    category = django_filters.CharFilter(field_name='category', lookup_expr='iexact', method='filter_names')
     name = django_filters.CharFilter(field_name='name', method='filter_names')
     price = django_filters.BaseRangeFilter(field_name='price')
     discount = django_filters.NumberFilter(field_name='discount', lookup_expr='gte')
